02eef19ee8c12d1610279952b0926b7727eaac27
MarcoAlvarezM <malvarez90617@ufide.ac.cr>
2024-06-27 15:25:43 -0600

    Quiz2

---
+++ Quiz2/src/quiz2/Quiz2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quiz2;
+
+/**
+ *
+ * @author Marco
+ */
+public class Quiz2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Importe de cancion
+        Cancion cancion = new Cancion("1234567", "A Little Messed Up", "June", "2:51", 2019);
+
+        // Salida de datos
+        System.out.println("El ID de la cancion es: " + cancion.getId());
+        System.out.println("El nombre de la cancion es: " + cancion.getTitulo());
+        System.out.println("El autor de la cancion es: " + cancion.getAutor());
+        System.out.println("La duracion de la cancion es: " + cancion.getDuracion());
+        System.out.println("El anio de creacion es: " + cancion.getAnio());
+    }
+}
+
